7b022ab988afe34a3d265a69e1a1d3e7201233cd
Quentin TARDIVON <quentin.tardivon@gmail.com>
2016-12-10 23:40:15 +0100

    Clean du maven pour les dépendances, test de JavaFX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.application.Application;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -10,24 +11,22 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Main extends Application {
-
+    private Scene scene;
     @Override public void start(Stage stage) {
 
-        Group root = new Group();
-        Scene scene = new Scene(root, 200, 150);
-        scene.setFill(Color.LIGHTGRAY);
-
-        Circle circle = new Circle(60, 40, 30, Color.GREEN);
-
-        Text text = new Text(10, 90, "JavaFX Scene");
-        text.setFill(Color.DARKRED);
-
-        Font font = new Font(20);
-        text.setFont(font);
-
-        root.getChildren().add(circle);
-        root.getChildren().add(text);
+        /*// create the scene
+        stage.setTitle("Web View");
+        scene = new Scene(new Browser(),750,500, Color.web("#666970"));
         stage.setScene(scene);
+        scene.getStylesheets().add("webviewsample/BrowserToolbar.css");
+        stage.show();*/
+
+
+        WebView webview = new WebView();
+        webview.getEngine().load(
+                Main.class.getResource("/player.html").toExternalForm()
+        );
+        stage.setScene(new Scene(webview));
         stage.show();
     }
 
