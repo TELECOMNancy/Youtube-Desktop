869d07d69fafec07eeeef5e4dcb10ab35c185f89
Quentin TARDIVON <quentin.tardivon@gmail.com>
2016-12-12 10:37:05 +0100

    Modif du main (test)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,9 @@
-package sample;
+package main;
 
 import javafx.application.Application;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 
 public class Main extends Application {
     private Scene scene;
@@ -21,12 +16,14 @@ public class Main extends Application {
         scene.getStylesheets().add("webviewsample/BrowserToolbar.css");
         stage.show();*/
 
-
         WebView webview = new WebView();
         webview.getEngine().load(
-                Main.class.getResource("/player.html").toExternalForm()
+                main.Main.class.getResource("/player.html").toExternalForm()
         );
-        stage.setScene(new Scene(webview));
+        String test = "M7lc1UVf-VE";
+        Player player = new Player("M7lc1UVf-VE");
+
+        stage.setScene(new Scene(player));
         stage.show();
     }
 
