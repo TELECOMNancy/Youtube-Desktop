21ba94e00e6b3243ca11f086c86fba73ff42d118
Maxime Escamez <maxime-escamez@telecomnancy.net>
2016-12-14 13:22:14 +0100

    commit mise a niveau

--- src/model/Model.java
+++ src/model/Model.java
@@ -5,19 +5,17 @@ import com.google.api.services.youtube.model.*;
 /**
  * Created by quentin on 13/12/2016.
  */
+
 public class Model {
 
 
     public String getVideoTitle(SearchResult video) {
-
         return video.getSnippet().getTitle();
     }
-
     public String getVideoID(SearchResult video){
         String rId = video.getId().getVideoId();
         return rId;
     }
-
     public String getVideoThumbnail(SearchResult video){
         Thumbnail thumbnail = video.getSnippet().getThumbnails().getDefault();
         return thumbnail.getUrl();
@@ -26,13 +24,14 @@ public class Model {
 
 
     public String getVideoTitle(PlaylistItem video) {
-
         return video.getSnippet().getTitle();
     }
+
     public String getVideoID(PlaylistItem video){
         String rId = video.getContentDetails().getVideoId();
         return rId;
     }
+
     public String getVideoThumbnail(PlaylistItem video){
         Thumbnail thumbnail = video.getSnippet().getThumbnails().getDefault();
         return thumbnail.getUrl();
