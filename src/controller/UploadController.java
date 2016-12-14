e3017ae189cfcd7528d3225a0d48f3c996abf725
Maxime Escamez <maxime-escamez@telecomnancy.net>
2016-12-14 11:41:49 +0100

    Commit du UploadModel et du mainTerminal

---
+++ src/controller/UploadController.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by madmax on 14/12/2016.
+ */
+public class UploadController {
+}
---
+++ src/model/UploadModel.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by madmax on 14/12/2016.
+ */
+public class UploadModel {
+}
